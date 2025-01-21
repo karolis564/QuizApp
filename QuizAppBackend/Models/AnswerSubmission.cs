cfc24f93b60ffbb0489348a6a68595686cdcb20d
karolis564 <105392010+karolis564@users.noreply.github.com>
2025-01-21 18:55:53 +0200

    Update AnswerSubmission.cs

--- QuizAppBackend/Models/AnswerSubmission.cs
+++ QuizAppBackend/Models/AnswerSubmission.cs
@@ -9,12 +9,12 @@ namespace QuizAppBackend.Models
 }
     public class Answer
     {
-        public int Id { get; set; } // This will be the primary key
+        public int Id { get; set; } 
         public string AnswerText { get; set; }
         public int QuizId { get; set; }
-        public Quiz? Quiz { get; set; } // Relationship with Quiz entity
-        public int PointCollected { get; set; } // Points awarded for this answer
-    } // Closing bracket for Answer class
+        public Quiz? Quiz { get; set; } 
+        public int PointCollected { get; set; } 
+    } 
 
   
 }
